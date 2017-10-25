68eb87b0861dfd8a8804109ab4302a49048ca09b
Siddappa Sandur <siddappa@prodevans.com>
2017-10-25 13:11:19 +0530

    updated the code

--- src/test/java/com/pack/home/TestExecution/ExtentManager.java
+++ src/test/java/com/pack/home/TestExecution/ExtentManager.java
@@ -11,7 +11,7 @@ public class ExtentManager {
 			extent = new ExtentReports(System.getProperty("user.dir") + "/test-output/ZenoExtentReport.html", true);
 			extent.loadConfig(new File(System.getProperty("user.dir") + "\\extent-config.xml"));
 			extent
-			.addSystemInfo("Host Name", "http://localhost:8080/zeno")
+			.addSystemInfo("Host Name", "http://localhost:8082/zeno/login")
 			.addSystemInfo("Environment", "DEMO")
 			.addSystemInfo("Application", "Zeno");
 		}
--- src/test/java/com/pack/home/TestExecution/TestRun.java
+++ src/test/java/com/pack/home/TestExecution/TestRun.java
@@ -20,135 +20,184 @@ public class TestRun {
 		driver = Dlib.openBrowser("chrome");
 		lib = new LibraryForGenericFunction(driver);
 		// Application link will be opened
-		driver.get("http://localhost:8080/zeno/login");
-	}
-
-	@Test(priority = 1, description="Performs an unsuccessful login and checks the resulting error message (passes)")
-	public void unsuccessfullogin() throws InterruptedException, IOException {
-		lib.jsSendKeysForID("username", "ROL000033", "login", "ID");
-		lib.jsSendKeysForID("password", "123456", "login", "ID");
-		Assert.assertTrue(lib.waitAndClickForID("login", "login", "ID"));
-		Thread.sleep(2000);
-		Assert.assertTrue(lib.getText("error", "login", "ID").contains("login fail"));					
+		driver.get("http://localhost:8082/zeno/login");
 	}
 	
 	@Test(priority = 2, description="Performs an successful login and checks the dashboard url (passes)")
-	public void login() throws InterruptedException, IOException {
-		lib.jsSendKeysForID("username", "ROL000007", "login", "ID");
-		lib.jsSendKeysForID("password", "123456", "login", "ID");
-		Assert.assertTrue(lib.waitAndClickForID("login", "login", "ID"));
-		Thread.sleep(2000);
-		Assert.assertEquals(lib.getCurrentUrl(), "http://localhost:8080/zeno/dashboard");					
+    public void login() throws InterruptedException, IOException {
+        lib.jsSendKeysForID("username", "OE-000003", "login", "ID");
+        lib.jsSendKeysForID("password", "123456", "login", "ID");
+        Assert.assertTrue(lib.waitAndClickForID("login", "login", "ID"));
+        Thread.sleep(2000);
+        //Assert.assertEquals(lib.getCurrentUrl(), "");                    
+    }
+	
+	
+	@Test(priority = 2, description="Tries to navigate to Parental Control (passes)")
+	public void parentallink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("parentallink", "dashboard", "linktext");
+	}
+	/*
+	@Test(priority = 3, description="Tries to navigate to Parental Control (passes)")
+	public void advancedlink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("advancedlink", "dashboard", "ID");
+		
+		
+		
+		@Test(priority = 4, description="Tries to navigate to Blocked List (passes)")
+	public void selectlink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("selectlink", "dashboard", "ID");
+	}
+	}*/
+	
+	
+	@Test(priority = 4, description="Tries to navigate to Blocked List (passes)")
+	public void advancedlink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("advancedlink", "dashboard", "ID");
 	}
 	
-	@Test(priority = 3,description="Checks for the welcome message (passes)")
-	public void verifyWelcomeMesg() throws InterruptedException, IOException {
-		Assert.assertEquals(lib.getText("welcomemesg", "dashboard", "class"), "Welcome VAMSI, to your One8 page");			
-	}	
+	@Test(priority = 4, description="Tries to navigate to Advanced List (passes)")
+	public void selectlink() throws InterruptedException, IOException {
+		try {
+			Thread.sleep(1000);
+			((WebDriver) lib).findElement(By.cssSelector("label[for='auctions']")).click();
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		//lib.waitAndClickForID("selectlink", "dashboard", "ID");
+	}
 	
-	@Test(priority = 4, description="Tries to navigate to privacy Page(passes)")
+	
+	@Test(priority = 4, description="Tries to navigate to Blocked List (passes)")
+	public void selectlink2() throws InterruptedException, IOException {
+		try {
+			Thread.sleep(1000);
+			((WebDriver) lib).findElement(By.cssSelector("label[for='abortion']")).click();
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		//lib.waitAndClickForID("selectlink", "dashboard", "ID");
+	}
+
+	
+	
+	@Test(priority = 4, description="Tries to navigate to Blocked List (passes)")
+	public void blockedlink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("blockedlink", "dashboard", "ID");
+	}
+	
+	@Test(priority = 5, description="Tries to navigate to Custom List (passes)")
+	public void customlink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("customlink", "dashboard", "ID");
+	}
+	
+	@Test(priority = 8, description="Perform Custom blocking of email (passes)")
+	public void Customurl() throws InterruptedException, IOException {
+		lib.jsSendKeysForID("url", "www.facebook.com", "dashboard", "ID");
+		lib.waitAndClickForID("Customurl", "dashboard", "ID");
+	}
+	
+	
+	@Test(priority = 9, description="Tries to navigate to Schedule part (passes)")
+	public void Schedulelink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("Schedulelink", "dashboard", "ID");
+	}
+	
+	@Test(priority = 10, description="Tries to navigate to Time schedule tab (passes)")
+	public void timeschedule1() throws InterruptedException, IOException {
+		lib.waitAndClickForID("timeschedule1", "dashboard", "ID");
+	}
+	
+	@Test(priority = 11, description="Tries to pass value to Time schedule tab (passes)")
+	public void timeblock() throws InterruptedException, IOException {
+		lib.jsSendKeysForID("timeslot1", "17:10", "dashboard", "ID");
+		lib.jsSendKeysForID("timeslot2", "11:10", "dashboard", "ID");
+		lib.waitAndClickForID("timeblock", "dashboard", "ID");
+	}
+	
+	/*@Test(priority = 10, description="Tries to navigate to Day schedule tab (passes)")
+	public void dayschedule() throws InterruptedException, IOException {
+		lib.waitAndClickForID("dayschedule", "dashboard", "ID");
+	}*/
+	
+	@Test(priority = 11, description="Tries to pass value to Day schedule tab (passes)")
+	public void Paybilllink() throws InterruptedException, IOException {
+		lib.jsSendKeysForID("timeslot3", "17:10", "dashboard", "ID");
+		lib.jsSendKeysForID("timeslot4", "11:10", "dashboard", "ID");
+		//lib.waitAndClickForID("timeblock", "dashboard", "ID");
+	}
+	
+	/*@Test(priority = 10, description="Tries to navigate to dates schedule tab (passes)")
+	public void dateschedule() throws InterruptedException, IOException {
+		lib.waitAndClickForID("dateschedule", "dashboard", "ID");
+	}*/
+	
+	/*
+	
+	
+	@Test(priority = 4, description="Tries to navigate to Contact US Page (passes)")
+	public void CUlink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("CUlink", "login", "linktext");
+	}
+	/*
+	@Test(priority = 5, description="Tries to navigate to Myone8 Page (passes)")
+	public void myone8link() throws InterruptedException, IOException {
+		lib.waitAndClickForID("myone8link", "login", "linktext");
+	}*/
+	
+/*
+	@Test(priority = 6, description="Perform Send Mail for Contact US  (passes)")
+	public void sendlink() throws InterruptedException, IOException {
+		lib.jsSendKeysForID("name", "Test", "login", "ID");
+		lib.jsSendKeysForID("email", "Test@test.com", "login", "ID");
+		lib.jsSendKeysForID("subject", "Test", "login", "ID");
+		lib.jsSendKeysForID("message", "Testing contsct us", "login", "ID");
+		Assert.assertTrue(lib.waitAndClickForID("sendlink", "login", "ID"));
+		Thread.sleep(2000);
+		//Assert.assertEquals(lib.getCurrentUrl(), "http://marcus.oneeight.co.in/zeno/dashboard");					
+	}
+	
+
+	@Test(priority = 7, description="Tries to navigate to New Connection Page (passes)")
+	public void NClink() throws InterruptedException, IOException {
+		lib.waitAndClickForID("NClink", "login", "linktext");
+	}
+
+	@Test(priority = 8, description="Perform Send Mail for New Connection (passes)")
+	public void Nconlink() throws InterruptedException, IOException {
+		lib.jsSendKeysForID("name", "Test", "login", "ID");
+		lib.jsSendKeysForID("email", "Test@test.com", "login", "ID");
+		lib.jsSendKeysForID("mobile", "9876543210", "login", "ID");
+		lib.jsSendKeysForID("city", "Testin", "login", "ID");
+		lib.jsSendKeysForID("pincode", "560076", "login", "ID");
+		lib.waitAndClickForID("Nconlink", "login", "ID");
+	}
+	
+		
+	@Test(priority = 9, description="Tries to navigate to privacy (passes)")
 	public void clickPrivacy() throws InterruptedException, IOException {
 		lib.waitAndClickForID("privacylink", "login", "linktext");
 	}
 	
-	@Test(priority = 5, description="Tries to navigate to Terms of services (passes)")
+	@Test(priority = 10, description="Tries to navigate to Terms And Condition (passes)")
 	public void clickTerms() throws InterruptedException, IOException {
-		lib.waitAndClickForID("termslink", "login", "linktext");
+		lib.waitAndClickForID("clickTerms", "login", "linktext");
 	}
 	
-	@Test(priority = 6, description="Tries to navigate to Refund page (passes)")
+	@Test(priority = 11, description="Tries to navigate to Refund page (passes)")
 	public void clickRefund() throws InterruptedException, IOException {
 		lib.waitAndClickForID("refundlink", "login", "linktext");
 	}
-	
-	@Test(priority = 7, description="Tries to navigate to Feedback (passes)")
-	public void clickfeedback() throws InterruptedException, IOException {
-		lib.waitAndClickForID("feedbacklink", "login", "linktext");
-	}
-	
-	@Test(priority = 8, description="Performs an Feedback send mail  (passes)")
-	public void sendfeedback() throws InterruptedException, IOException {
-		lib.jsSendKeysForID("subject", "test", "login", "ID");
-		lib.jsSendKeysForID("message", "testing1feedback", "login", "ID");
-		Assert.assertTrue(lib.waitAndClickForID("sendnowfeedback", "login", "ID"));
-		Thread.sleep(2000);
-		//Assert.assertEquals(lib.getCurrentUrl(), "http://localhost:8080/zeno/contactusPage");					
-	}
-	
-	@Test(priority = 9, description="Tries to navigate to FAQ (passes)")
+			
+	@Test(priority = 12, description="Tries to navigate to FAQ (passes)")
 	public void clickfaq() throws InterruptedException, IOException {
 		lib.waitAndClickForID("faqlink", "login", "linktext");
 	}
-	@Test(priority = 10, description="Tries to navigate to Know your CPE (passes)")
-	public void clickknowcpe() throws InterruptedException, IOException {
-		lib.waitAndClickForID("knowlink", "login", "linktext");
-	}
-	
-	@Test(priority = 11, description="Tries to navigate to Know your BILL (passes)")
-	public void clickknowbill() throws InterruptedException, IOException {
-		lib.waitAndClickForID("knowbilllink", "login", "linktext");
-	}
-	
-	@Test(priority = 12, description="Tries to navigate to Make Payment (passes)")
-	public void clickPayment() throws InterruptedException, IOException {
-		lib.waitAndClickForID("paymentlink", "dashboard", "linktext");
-	}
-
-	@Test(priority = 13, description="Tries to navigate to pay Bill (passes)")
-	public void clickPayBill() throws InterruptedException, IOException {
-		lib.waitAndClickForID("paybilllink", "dashboard", "linktext");
-	}
-	
-	@Test(priority = 14, description="Tries to navigate to Contact us (passes)")
-	public void clickcontact() throws InterruptedException, IOException {
-		lib.waitAndClickForID("contactlink", "login", "linktext");
-	}
-	
-	@Test(priority = 15, description="Performs an contactus send mail  (passes)")
-	public void sendcontactus() throws InterruptedException, IOException {
-		lib.jsSendKeysForID("subject", "test", "login", "ID");
-		lib.jsSendKeysForID("message", "testingcontact", "login", "ID");
-		Assert.assertTrue(lib.waitAndClickForID("sendnow", "login", "ID"));
-		Thread.sleep(1000);
-		//Assert.assertEquals(lib.getCurrentUrl(), "http://localhost:8080/zeno/contactusPage");					
-	}
-	
-	
-	@Test(priority = 16, description="Tries to navigate to Top up (passes)")
-	public void clickTopUp() throws InterruptedException, IOException {
-		//Assert.assertEquals(lib.getText("welcomemesg", "popup", "class"), "Welcome Saravanan, to your One8 page");
-		lib.waitAndClickForID("topuplink", "dashboard", "ID");
-	}
-	
-	@Test(priority = 17, description="Tries to navigate to Your Plan(passes)")
-	public void clickPlan() throws InterruptedException, IOException {
-		lib.waitAndClickForID("planlink", "dashboard", "ID");
-	}
-
-	@Test(priority = 18, description="Navigates to Profile page (passes)")
-	public void clickProfile() throws InterruptedException, IOException {
-		lib.waitAndClickForID("profilelink", "dashboard", "linktext");
-	}
-	
-	@Test(priority = 19, description="Navigates to Service request page (passes)")
-	public void clickService() throws InterruptedException, IOException {
-		lib.waitAndClickForID("servicelink", "dashboard", "linktext");
-	}
-	
-	@Test(priority = 20, description="Performs an Service request send mail  (passes)")
-	public void sendnowrequest() throws InterruptedException, IOException {
-		//lib.jsSendKeysForID("check", "New connection", "login", "ID");
-		lib.jsSendKeysForID("message", "tellus", "login", "ID");
-		Assert.assertTrue(lib.waitAndClickForID("sendnowrequest", "login", "ID"));
-		Thread.sleep(2000);
-		//Assert.assertEquals(lib.getCurrentUrl(), "http://localhost:8080/zeno/contactusPage");					
-	}
-
-	@Test(priority = 21, description="Logs out (passes)")
-	public void logout() throws InterruptedException, IOException {
-		lib.waitAndClickForID("logoutlink", "dashboard", "linktext");
-	}
-	
+	*/
 	@AfterTest()
 	public void afterMethod() throws IOException {
 		driver.quit();		
